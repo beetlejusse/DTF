3a8e279f3338185f63d4b1422fe2909b5036d8b2
Utsav Jana <utsav.jana.5@gmail.com>
2025-09-10 13:18:53 +0530

    added function to buy tokens for the users ETH

--- contracts/src/DTFContract.sol
+++ contracts/src/DTFContract.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.19;
 
 import "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
-import "../universal-router/contracts/UniversalRouter.sol";
-import "../lib/v4-core/src/PoolManager.sol";
+import "../lib/universal-router/contracts/UniversalRouter.sol";
+import "../lib/universal-router/contracts/libraries/Commands.sol";
+import "../lib/v4-periphery/src/interfaces/IV4Router.sol";
+import "../lib/v4-core/src/interfaces/IPoolManager.sol";
+import "../lib/v4-core/src/libraries/StateLibrary.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 import "./utils/DTFConstants.sol";
@@ -14,15 +18,26 @@ contract DTFContract is DTFConstants, ReentrancyGuard, ERC20, Ownable{
 
     //EVENTS
     event DTFTokensMinted(uint256 investedETH, uint256 dtfTokensMinted, address indexed to);
+    event TokenSwapped(address indexed token, uint256 ethSpent, uint256 tokensReceived);
+    event TokensRedeemed(address indexed user, uint256 dtfTokensBurned, uint256 ethRedeemed);  
+
+    //STRUCTS
+    struct UniswapV4Addresses {
+        address poolManager;
+        address universalRouter;
+    }
 
     //CONSTANTS
     UniversalRouter public immutable universalRouter;
-    PoolManager public immutable poolManager;
+    IPoolManager public immutable poolManager;
 
-    address[] public immutable tokens;
-    uint256[] public immutable weights;
+    address[] public tokens;
+    uint256[] public weights;
+
+    mapping (address => uint256) public tokenBalance;
+
     uint256 public immutable createdAt;
-    uint256 public totalInvested;
+    uint256 public totalValueLocked;
     uint256 public totalFeesCollected;
     
     constructor(
@@ -32,18 +47,17 @@ contract DTFContract is DTFConstants, ReentrancyGuard, ERC20, Ownable{
         uint256[] memory _weights,
         uint256 _createdAt,
         address _creator,
-        address _universalRouter,
-        address _poolManager        
+        UniswapV4Addresses memory _deployment
     ) ERC20(_name, _symbol) Ownable(_creator){
         
         require(_tokens.length == _weights.length, "lenght mismatch");
-        require(_tokens.lenght >1, "required atleast 2 tokens");
+        require(_tokens.length >1, "required atleast 2 tokens");
 
         tokens=_tokens;
         weights=_weights;
         createdAt=_createdAt;
-        universalRouter = UniversalRouter(_universalRouter);
-        poolManager = PoolManager(_poolManager);  
+        universalRouter = UniversalRouter(payable(_deployment.universalRouter));
+        poolManager = IPoolManager(_deployment.poolManager);  
     }
 
     receive() external payable{
@@ -56,11 +70,31 @@ contract DTFContract is DTFConstants, ReentrancyGuard, ERC20, Ownable{
         _mintWithEth(msg.value, msg.sender);
     }
 
+    function redeemforEth(uint256 dtfAmount) external nonReentrant{
+        require(balanceOf(msg.sender) >= dtfAmount, "insufficient DTF balance");
+        require(dtfAmount > 0, "invalid amount");
+
+        uint256 fee= (dtfAmount * REDEEM_FEE_BPS)/BASIC_POINTS;
+        uint256 userShareBPS= (dtfAmount * BASIC_POINTS) / totalSupply(); //gives what percentage of the total supply the user is redeeming 
+
+        uint256 ethRedeemed= _sellUnderlyingTokensForETH(userShareBPS);
+        uint256 redeemAmount= ethRedeemed - fee;
+
+        _burn(msg.sender, dtfAmount);
+
+        totalValueLocked -= ethRedeemed;
+        totalFeesCollected += fee;
+
+        (bool success, ) = msg.sender.call{value: redeemAmount}("");
+        require(success, "ETH transfer failed");
+
+        emit TokensRedeemed(msg.sender, dtfAmount, redeemAmount);
+    }
 
     //INTERNAL FUNCTIONS
     function _mintWithEth(uint256 amount, address to) internal{
         uint256 fees = (amount* MINT_FEES_BPS)/BASIC_POINTS;
-        uint256 investedAmount= amount - fees;
+        uint256 investedAmount= amount - fee;
 
         //buy underlying assets using the universal router
         _buyUnderlyingTokensWithETH(investedAmount);
@@ -70,19 +104,125 @@ contract DTFContract is DTFConstants, ReentrancyGuard, ERC20, Ownable{
         _mint(to, dtfTokensToMint);
 
         //update state variables
-        totalInvested += investedAmount;
-        totalFeesCollected += fees;
+        totalValueLocked += investedAmount;
+        totalFeesCollected += fee;
 
-        emit DTFTokensMinted(totalInvested, dtfTokensToMint, to);
+        emit DTFTokensMinted(totalValueLocked, dtfTokensToMint, to);
     }
 
     function _buyUnderlyingTokensWithETH(uint256 amount) internal{
 
+        for(uint256 i; i< tokens.length; i++){
+            address token= tokens[i];
+            uint256 weight= weights[i];
+            uint256 ethForToken= (amount * weight)/BASIC_POINTS;
+
+            //now calling the swap function only for erc20 tokens
+            if (token= address(0)){
+                tokenBalance(address(0)) += ethForToken; //not doing anything with eth.. is stored in the contract for transactions and gas
+            } else {
+                uint256 tokenAmount= _swapETHForTokenv4(token, ethForToken);
+                tokenBalance[token] += tokenAmount;
+
+                emit TokenSwapped(token, amount, tokenAmount);
+            }
+        }
     }
+
+    function _sellUnderlyingTokensForETH(uint256 dtfAmount) internal returns(uint256 ethRedeemed){
+        require(dtfAmount > 0, "invalid amount");
+
+        for(uint256 i; i< tokens.length; i++){
+
+            uint256 tokenAmountToSell= (tokenBalance[tokens[i]] * dtfAmount)/ totalSupply();    //calculates amount of token to sell based on the user's share of total supply
+            
+            if( tokens[i]== address(0)){
+                ethRedeemed += tokenAmountToSell;
+            } else {
+                uint256 ethRecievedFromSwap= _swapTokenForETHV4(tokens[i], tokenAmountToSell);
+                ethRedeemed += ethRecievedFromSwap;
+            }
+
+            tokenBalance[tokens[i]] -= tokenAmountToSell;
+        }
+    }
+
+    function _swapETHForTokenv4(address token, uint256 ethAmount) internal returns(uint256 tokensReceived){
+
+        require(ethAmount > 0, "invalid amount");
+
+
+        //approve universal router to spend tokens
+        IERC20(token).approve(address(universalRouter), type(uint256).max);
+
+        //Define PoolKey for the ETH/token pair
+        PoolKey memory poolKey=({
+            currency0: Currency(address(0)),    //the ETH currency getting accepted
+            currency1: Currency(token),         //the token currency getting swapped to
+            fee: 3000,    
+            tickSpacing: 60,                    // Tick spacing for the fee tier      
+            hooks: address(0)                   //no hooks for this basic swap             //the fee tier of the pool
+        });
+
+        //define actions for the swap using the singlton architecture of v4
+        bytes memory commands = abi.encodePacked(uint8(Commands.V4_SWAP));  //what the universal router will execute
+
+        bytes memory actions= abi.encodePacked(
+            uint8(Actions.SWAP_EXACT_IN_SINGLE),    //this ensures exact amount of eth is swapped
+            uint8(Actions.SETTLE_ALL),              //flash accounting settles the swap interanlly
+            uint8(Actions.TAKE_ALL)                 //take all output tokens from the swap and send to this contract
+        );
+
+        //define the parameters for the swap
+        bytes[] memory params= new bytes[](3);
+
+        IV4Router.ExactInputSingleParams memory swapParams= IV4Router.ExactInputSingleParams({
+            poolKey: poolKey,
+            zeroForOne: true,                       //boolean determines the direction of the swap,swapping from currency0 to currency1
+            amountIn: uint128(ethAmount),              //amount of eth to swap
+            amountOutMinimum: 0,                    //no slippage protection for now
+            hookData: bytes("")                     //no hook data
+        })
+
+        params[0]= abi.encode(swapParams);                  //params for swap function
+        params[1] = abi.encode(poolKey.currency0, ethAmount);  //encoding of the currency to be debited and the amount to be paid
+        params[2] = abi.encode(poolKey.currency1);          //encoding of the currency to be received 
+
+        //combines these params and actions into a single input for the universal router
+        bytes[] memory inputs= new bytes[](1);
+        inputs[0]= abi.encode(actions, params);
+
+        //Execute the swap through the Universal Router
+        uin256 tokensBeforeSwap= IERC20(token).balanceOf(address(this));
+
+        uint256 deadline = block.timestamp + DEFAULT_SWAP_DEADLINE; //how long the swap is valid for
+        universalRouter.execute{value: ethAmount}(commands, inputs, deadline); //execute the swap
+
+        uint256 tokenBalanceAfter = IERC20(token).balanceOf(address(this));
+
+        tokensReceived = tokenBalanceAfter - tokensBeforeSwap;
+
+        require(tokensReceived > 0, "No tokens received from swap");        
+    }
+
+    function _swapTokenForETHV4(address token, uint256 tokenAmount){
+        require(tokenAmount > 0, "invalid amount");
+
+
+
+        
+    }
+
+    
 
     function _calculateDTFTokensToMint(uint256 amount) internal view returns(uint256){
         
-        return amount;
-    }
+        ig(totalSupply() == 0){
 
+            return amount;
+        } else {
+
+            return (amount * totalSupply()) / totalValueLocked;
+        }
+    }
 }
--- contracts/src/utils/DTFConstants.sol
+++ contracts/src/utils/DTFConstants.sol
@@ -9,11 +9,14 @@ abstract contract DTFConstants{
     uint256 internal constant BASIC_POINTS=10000;
 
     //DTFCONTRACT CONSTANTS
-    uint256 internal constant MINT_FEES_BPS= 50; //0.5% fees
-    uint256 internal constant REDEEM_FEE_BPS = 50; // 0.5% fees
+    uint256 internal constant MINT_FEES_BPS= 30; //0.3% fees
+    uint256 internal constant REDEEM_FEE_BPS = 30; // 0.3% fees
+    uint256 internal constant DEFAULT_SWAP_DEADLINE = 60; //1 min
 
     //HELPER CONFIG CONSTANTS
-
+    address internal constant POOL_MANAGER_MAINNET = 0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32;
+    address internal constant UNIVERSAL_ROUTER_MAINNET = 0xA51afAFe0263b40EdaEf0Df8781eA9aa03E381a3;
+    
     
 
     
